e82feeaf526e7071a3e55f20d0ed5ab7431a7beb
shunak <shuna966@gmail.com>
2019-08-31 01:07:10 +0900

    190830

---
+++ enum.ts
@@ -0,0 +1,35 @@
+//enumeration type
+
+//enum Signal{
+//
+//    Red =0,
+//    Blue =1,
+//    Yellow=2
+//
+//}
+//
+
+
+enum Signal{
+
+    Red,
+    Blue =3,
+    Yellow
+
+}
+
+
+
+enum Signal{
+
+    Green=5
+
+}
+
+
+
+var results: Signal;
+console.log(Signal.Green);
+
+
+
